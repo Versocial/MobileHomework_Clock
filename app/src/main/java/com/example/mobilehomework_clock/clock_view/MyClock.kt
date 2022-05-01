9df77bf1b3065cadb9ba5be3be7942cb16b0b5c1
Versocial <whx_albert@bupt.edu.cn>
2022-05-01 12:46:12 +0800

    clock画完

--- app/src/main/java/com/example/mobilehomework_clock/clock_view/MyClock.kt
+++ app/src/main/java/com/example/mobilehomework_clock/clock_view/MyClock.kt
@@ -1,7 +1,10 @@
 package com.example.mobilehomework_clock.clock_view
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
@@ -9,7 +12,8 @@ import android.view.View
  * TODO: document your custom view class.
  */
 class MyClock : View {
-
+    private val widthOfCircle:Float=0.08f
+    private val defaultWidth=200
     constructor(context: Context) : super(context) {
         init(null, 0)
     }
@@ -26,8 +30,36 @@ class MyClock : View {
 
     }
 
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        setMeasuredDimension(measure(widthMeasureSpec), measure(heightMeasureSpec))
+    }
 
+    private fun measure(origin: Int): Int {
+        var result: Int = defaultWidth
+        val specMode = MeasureSpec.getMode(origin)
+        val specSize = MeasureSpec.getSize(origin)
+        if (specMode == MeasureSpec.EXACTLY) {
+            result = specSize
+        } else {
+            if (specMode == MeasureSpec.AT_MOST) {
+                result = Math.min(result, specSize)
+            }
+        }
+        return result
+    }
+
+    @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas) {
+        val paint: Paint= Paint()
+        paint.color=Color.BLACK
+        paint.isAntiAlias=true
+        paint.strokeWidth= (widthOfCircle*Math.min(width/2,height/2))*widthOfCircle
+        val r=(Math.min(width/2,height/2))-paint.strokeWidth
+        paint.style=Paint.Style.STROKE
+        canvas.drawCircle((width/2).toFloat(), (height/2).toFloat(),
+                r,paint)
+
+
         super.onDraw(canvas)
 
     }
